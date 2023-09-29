9ebed0505061657510e906aa6cc055de180907e7
keishiin <inferno2960@gmail.com>
2023-09-29 12:25:52 -0400

    added reddis session storage for sigining in and signing out

--- src/api/auth.rs
+++ src/api/auth.rs
@@ -1,4 +1,7 @@
 use actix_web::web;
+use actix_session::Session;
+use uuid::Uuid;
+use serde_json::json;
 use actix_web::{web::Json, get, post, HttpResponse};
 
 use crate::models::users::{User, UserRequest};
@@ -25,7 +28,7 @@ async fn signup(new_user: Json<User>) -> HttpResponse {
 }
 
 #[post("/signin")]
-async fn signin(user_info: Json<UserRequest>) -> Result<HttpResponse, ApiError> {
+async fn signin(user_info: Json<UserRequest>, session: Session) -> Result<HttpResponse, ApiError> {
     let user = User::get_user_by_username(&user_info.username)
         .map_err(|e| {
             match e.status_code {
@@ -37,11 +40,26 @@ async fn signin(user_info: Json<UserRequest>) -> Result<HttpResponse, ApiError> 
     let is_correct_user = user.verify_password(user_info.password.as_bytes())?;
     
     if is_correct_user {
+        session.insert("user_id", &user.id).expect("session insert error");
+        session.renew();
         Ok(HttpResponse::Ok().json(user))
     } else {
         Err(ApiError::new(401, "Credentials not valid!".to_string()))
     }
 }
+
+#[post("/signout")]
+async fn singout(session: Session) -> Result<HttpResponse, ApiError> {
+    let id: Option<Uuid> = session.get("user_id").expect("session get error");
+
+    if let Some(_) = id {
+        session.purge();
+        Ok(HttpResponse::Ok().json(json!({"message": "Signout success!"})))
+    } else { 
+        Err(ApiError::new(401, "Unauthorized".to_string()))
+    }
+}
+
 
 pub fn auth_config(cfg: &mut web::ServiceConfig) {
     cfg.service(
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[macro_use]
 extern crate log;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, middleware};
+use actix_session::{storage::RedisActorSessionStore, SessionMiddleware};
 use dotenv::dotenv;
 use listenfd::ListenFd;
 use std::env;
@@ -13,15 +14,29 @@ mod utils;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
+
     dotenv().ok();
     env_logger::init();
+
+    let redis_port = env::var("REDIS_PORT").expect("Redis port not set");
+    let redis_host = env::var("HOST").expect("Redis host not set");
 
     let user_db = repository::database::Database::new();
     let app_data = web::Data::new(user_db);
 
+    let private_key = actix_web::cookie::Key::generate();
+
     let mut listenfd = ListenFd::from_env();
     let mut server = HttpServer::new( move || {
         App::new()
+            .wrap(
+                SessionMiddleware::builder(
+                    RedisActorSessionStore::new(format!("{}:{}", redis_host, redis_port)), 
+                    private_key.clone(),
+                )
+                .build(),
+            )
+            .wrap(middleware::Logger::default())
             .app_data(app_data.clone())
             .configure(api::api::config)
             .configure(api::auth::auth_config)
@@ -32,12 +47,12 @@ async fn main() -> std::io::Result<()>{
         Some(listener) => server.listen(listener)?,
         None => {
             let host = env::var("HOST").expect("Host not set");
-            let port = env::var("PORT").expect("Port not set");
+            let port = env::var("SERVER_PORT").expect("Port not set");
             server.bind(format!("{}:{}", host, port))?
         }
     };
 
-    info!("starting server");
+    log::info!("starting HTTP server at http://localhost:8080");
     server.run().await
 
 }

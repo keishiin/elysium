c0ea90149c4fb6bda757d3c6be2ed57b841edf98
keishiin <inferno2960@gmail.com>
2023-10-23 22:16:12 -0400

    added components dir and Header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header.tsx"
+
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold text-center"> Hello World! </h1>
+    <Header/>
   )
 }
 
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <h1 className="text-3xl font-bold text-center"> Hello World! </h1>
+  )
+}
+
+export default Header; 

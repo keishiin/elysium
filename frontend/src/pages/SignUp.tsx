0ffeca06c59bef8ae4a67e3427da1c1c82bf0523
keishiin <inferno2960@gmail.com>
2023-10-25 16:21:51 -0400

    added sign up page styling

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,5 +1,98 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function SignUp() {
-  return <div>Sign Up Page</div>;
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event: any) => {
+    console.log("username => ", username);
+    console.log("password => ", password);
+    console.log("passwordCheck => ", passwordCheck);
+    console.log("email => ", email);
+
+    event.target.reset();
+
+    setUserName("");
+    setPassword("");
+    setPasswordCheck("");
+    setEmail("");
+  };
+
+  return (
+    <div className="bg-gray-100 flex justify-center items-center h-screen">
+      {/* Right: Sign up Form  */}
+      <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+        <h1 className="text-2xl font-semibold mb-4">Sign Up</h1>
+        <form onSubmit={handleSubmit}>
+          {/* <!-- Username Input --> */}
+          <div className="mb-4">
+            <label className="block text-gray-600">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              onChange={(event) => setUserName(event.target.value)}
+            ></input>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-600">Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              onChange={(event) => setEmail(event.target.value)}
+            ></input>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-600">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              onChange={(event) => setPassword(event.target.value)}
+            ></input>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-600">Confirm Password</label>
+            <input
+              type="password"
+              id="confrimPassword"
+              name="confirmPassword"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              onChange={(event) => setPasswordCheck(event.target.value)}
+            ></input>
+          </div>
+          {/* <!-- Sign Up Button --> */}
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+          >
+            Sign Up
+          </button>
+        </form>
+        {/* <!-- Sign up  Link --> */}
+        <div className="mt-6 text-blue-500 text-center">
+          <Link to="/login" className="hover:underline">
+            Login
+          </Link>
+        </div>
+      </div>
+      {/* <!-- Right: Image --> */}
+      <div className="w-1/2 h-screen hidden lg:block">
+        <img
+          src="../../images/photo-1698207873249-640dab81d84e.jpg"
+          alt="Placeholder Image"
+          className="object-cover w-full h-full"
+        ></img>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;

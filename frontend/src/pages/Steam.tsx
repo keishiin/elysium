7ed734489ab1a4057cbd0b427788e22dcaf2b6d3
keishiin <inferno2960@gmail.com>
2024-03-17 01:29:37 -0400

    updates

--- frontend/src/pages/Steam.tsx
+++ frontend/src/pages/Steam.tsx
@@ -1,43 +1,67 @@
+'use client';
 import ErrorComponent from "../components/ErrorComponent";
 import Loading from "../components/Loading";
 import Navbar from "../components/Navbar";
-import { get_onwed_games } from "../services/steam";
-import { Avatar, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Pagination, Spinner } from "@nextui-org/react";
+import {
+    Avatar,
+    Table,
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell,
+    Pagination,
+    Spinner,
+} from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import timeConversion from "../utils/time";
 import IMAGE_URL_BASE from "../constants";
+import apiClient from "../services/api-common";
 
 function Steam() {
-    const [page, setPage] = useState(104);
-    const ownedGames = get_onwed_games(page);
-    const [data, setData] = useState<Game[]>([]);
+    const [page, setPage] = useState(0);
+    const [data, setData] = useState<Steam>({
+        cursor: 0,
+        data: [],
+        game_count: 0,
+    });
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(false);
+    const [pages, setPages] = useState(1);
+
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("user");
 
     useEffect(() => {
-        if (ownedGames.isLoading) {
+        const fetchData = async () => {
             setIsLoading(true);
             setIsError(false);
-        } else if (ownedGames.isError) {
+            try {
+                let url = `steam/games`;
+                if (page > 0) {
+                    url = `steam/games?cursor=${page}`;
+                }
+                const response = await apiClient.get(url, {
+                    headers: {
+                        "axum-accountId": userId,
+                        Authorization: token,
+                    },
+                });
+                setData(response.data);
+                setPages(Math.ceil(response.data.game_count / 10))
+            } catch (error) {
+                setIsError(true);
+            }
             setIsLoading(false);
-            setIsError(true);
-        } else {
-            setIsLoading(false);
-            setIsError(false);
-            setData(ownedGames.data?.data || []);
-        }
-    }, [ownedGames]);
+        };
 
-    const pages = 24;
-
-    useEffect(() => {
-        console.log(page)
-    }, [page])
+        fetchData();
+    }, [page, token, userId]);
 
     return (
         <>
             <Navbar />
-            <div className="flex justify-center items-center min-h-screen w-full bg-black text-white">
+            <div className="flex justify-center items-center min-h-screen w-full bg-gray-700 text-white">
                 {isLoading && <Loading />}
                 {isError && <ErrorComponent />}
                 {!isLoading && !isError && (
@@ -66,19 +90,29 @@ function Steam() {
                             <TableColumn key="playtime_forever">Total Time</TableColumn>
                         </TableHeader>
                         <TableBody
-                            items={data}
+                            items={data.data}
                             loadingContent={<Spinner />}
                             loadingState={isLoading ? "loading" : "idle"}
                         >
                             {(game) => (
                                 <TableRow key={game?.appid}>
-                                    <TableCell><Avatar isBordered className="opacity-100 z-100" src={`${IMAGE_URL_BASE}/${game.appid}/${game.img_icon_url}.jpg`}></Avatar></TableCell>
+                                    <TableCell>
+                                        <Avatar
+                                            isBordered
+                                            className="opacity-100 z-100"
+                                            src={`${IMAGE_URL_BASE}/${game.appid}/${game.img_icon_url}.jpg`}
+                                        ></Avatar>
+                                    </TableCell>
                                     <TableCell>{game.name}</TableCell>
                                     <TableCell>
-                                        {game.playtime_2weeks ? `${timeConversion(game.playtime_2weeks)} hrs` : "0 hrs"}
+                                        {game.playtime_2weeks
+                                            ? `${timeConversion(game.playtime_2weeks)} hrs`
+                                            : "0 hrs"}
                                     </TableCell>
                                     <TableCell>
-                                        {game.playtime_forever ? `${timeConversion(game.playtime_forever)} hrs` : "0 hrs"}
+                                        {game.playtime_forever
+                                            ? `${timeConversion(game.playtime_forever)} hrs`
+                                            : "0 hrs"}
                                     </TableCell>
                                 </TableRow>
                             )}
